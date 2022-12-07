30a8c53021d62c302d9b5452d6584c622028b5d5
Carla TODORAN-PESCARUS <carlatodoran1507@gmail.com>
2022-12-07 15:42:10 +0200

    All functions ready

--- RecDescentParser.py
+++ RecDescentParser.py
@@ -7,6 +7,16 @@ class ParserState(Enum):
     final = 3
     error = 4
 
+
+def find_index(list_prod, prod):
+    for index, element in enumerate(list_prod):
+        if element == prod:
+            if index + 1 > len(list_prod):
+                return -1
+            return list_prod[index + 1]
+    return -1
+
+
 class RecDescentParser:
     def __init__(self, grammar, input_seq):
         self.grammar = grammar
@@ -25,10 +35,26 @@ class RecDescentParser:
     # todo Carla: (expand, momentary insuccess, another try)
     def expand(self):
         non_terminal_value = self.input_stack.pop(0)
-        production = self.grammar.get_production_of_non_terminal(non_terminal_value)
+        production = self.grammar.get_production_of_non_terminal(non_terminal_value)[0]
         self.working_stack.append((non_terminal_value, production))
 
     def momentary_insuccess(self):
         self.state = ParserState.back
 
     def another_try(self):
+        production = self.working_stack.pop()
+        non_terminal_value = production[0]
+        productions = self.grammar.get_production_of_non_terminal(non_terminal_value)
+        result = find_index(productions, production[1])
+
+        initial_state = self.grammar.get_start_symbol()
+        if self.position_input == 1 and initial_state == non_terminal_value:
+            self.state = ParserState.error
+            raise Exception('Error')
+        elif result != -1:
+            self.state = ParserState.normal
+            self.working_stack.append((non_terminal_value, result))
+            self.input_stack.pop(0)
+            self.input_stack.insert(0, result)
+        else:
+            self.input_stack.insert(0, non_terminal_value)
