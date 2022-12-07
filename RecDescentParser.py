da7da8ab938ca237e73c894850393f741bf60466
Carla TODORAN-PESCARUS <carlatodoran1507@gmail.com>
2022-12-07 15:03:42 +0200

    Added two more methods

--- RecDescentParser.py
+++ RecDescentParser.py
@@ -7,7 +7,6 @@ class ParserState(Enum):
     final = 3
     error = 4
 
-
 class RecDescentParser:
     def __init__(self, grammar, input_seq):
         self.grammar = grammar
@@ -17,8 +16,19 @@ class RecDescentParser:
         self.input_stack = [grammar.get_start_symbol()]
         self.input_seq = input_seq
 
-    # todo me: (advance, back, success)
+    # todo Liviu: (advance, back, success)
     def advance(self):
         self.position_input += 1
         terminal_value = self.input_stack.pop(0)
         self.working_stack.append(terminal_value)
+
+    # todo Carla: (expand, momentary insuccess, another try)
+    def expand(self):
+        non_terminal_value = self.input_stack.pop(0)
+        production = self.grammar.get_production_of_non_terminal(non_terminal_value)
+        self.working_stack.append((non_terminal_value, production))
+
+    def momentary_insuccess(self):
+        self.state = ParserState.back
+
+    def another_try(self):
